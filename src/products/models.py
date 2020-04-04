0ad0e4c5b0b0636174b439932421c993630e927e
AdrianDucao <adrianducao@gmail.com>
2020-04-04 18:53:50 +0800

    changing models

--- src/products/models.py
+++ src/products/models.py
@@ -5,3 +5,4 @@ class Product(models.Model):
     title       = models.CharField(max_length=120)   
     description = models.TextField(blank=True, null=True)
     price       = models.DecimalField(decimal_places=2, max_digits=10000)
+    featured    = models.BooleanField()
